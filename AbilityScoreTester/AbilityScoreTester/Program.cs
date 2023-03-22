555cb0081b4a80c52b3509c53c4a802c969f5948
Marie Torsnes <marie.torsnes@gmail.com>
2023-03-22 12:54:14 +0100

    first commit

---
+++ AbilityScoreTester/AbilityScoreTester/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace AbilityScoreTester;
+class Program
+{
+    static void Main(string[] args)
+    {
+        AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+
+        while (true) {
+
+            calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+            calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+            calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+            calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+            calculator.CalculateAbilityScore();
+            Console.WriteLine("Calculated ability score: " + calculator.Score);
+            Console.WriteLine("Press Q to quit, any other key to continue");
+            char keychar = Console.ReadKey(true).KeyChar;
+            if ((keychar == 'Q') || (keychar == 'q')) return;
+        }
+
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]:");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine("   using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("   using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]:");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine("   using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("   using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
+
